b914ee30079478d99130be3e328aa98cddd98235
msndogui@hotmail.com.br <msndogui@hotmail.com.br>
2014-04-02 22:03:22 -0300

    Adicionando Projeto em C

---
+++ questao03.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+pid_t idProcesso;
+printf("Comecando o programa...\n");
+idProcesso = fork();
+int i;
+for(i = 0; i <= 60; i ++){
+	switch(idProcesso){
+		case -1:
+			printf("Houve erro na execucao");
+			exit(1);
+		if(i == 14){
+			case 0:
+				printf("Sou o processo IDPai1: %d\n", getpid());
+				if(i == 26){
+					idProcesso = fork();
+					switch(idProcesso){
+						case -1:
+							printf("Houve erro na execucao");
+							exit(1);
+						case 0:
+							printf("Sou o IDNeto1: %d\n", getpid());					
+						break;
+						default:
+							printf("Sou o processo Pai 1 de ID: %d, IDNeto1: %d\n", getpid(), idProcesso);
+						break;
+					}	
+				  }
+			break;
+		}
+		default:
+			printf("Sou o processo IDAvo: %d\n", getpid());
+			idProcesso = fork();
+			switch(idProcesso){
+				case -1:
+					printf("Houve erro na execucao");
+					exit(1);
+				if(i == 16){
+					case 0:
+						printf("Sou o IDPai2: %d\n", getpid());	
+						idProcesso = fork();
+						switch(idProcesso){
+							case -1:
+								printf("Houve erro na execucao");
+								exit(1);
+							case 0:
+								printf("Sou o IDNeto2: %d\n", getpid());					
+							break;
+							default:
+								printf("Sou o processo Pai 2 de ID: %d, IDNeto2: %d\n", getpid(), idProcesso);
+							break;
+						  }
+					break;
+				}
+				default:
+					printf("Sou o processo IDAvo de ID: %d, IDPai2: %d\n", getpid(), idProcesso);
+				break;
+			  }
+		break;
+ 	 }
+kill(getpid(), 0);
+}
+
+}
